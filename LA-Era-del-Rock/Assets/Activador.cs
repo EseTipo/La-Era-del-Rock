ae45c2bab8f711aa84c97a91d0a83f119e30f907
EseTipo <pbelmar@ing.ucsc.cl>
2022-01-03 00:10:40 -0300

    enviar
    repositorio

---
+++ LA-Era-del-Rock/Assets/Activador.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activador : MonoBehaviour
+{
+    bool active = false;
+    SpriteRenderer sr;
+    public KeyCode key;
+    GameObject note;
+    Color old;
+
+    void Awake(){
+        sr = GetComponent<SpriteRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        old = sr.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+            StartCoroutine(Pressed());
+
+        if(Input.GetKeyDown(key) && active){
+            Destroy(note);
+            StartCoroutine(Pressed());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col){
+        active = true;
+        if(col.gameObject.tag=="Note")
+            note=col.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D col){
+        active = false;
+    }
+
+    IEnumerator Pressed(){
+        
+        sr.color = new Color(0,0,0);
+        yield return new WaitForSeconds(0.05f);
+        sr.color = old;
+    }
+}
